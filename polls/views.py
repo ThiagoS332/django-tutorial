e6b69a36d12c1189d1a8663710386c467e0683ea
Thiago <thiago.soarescds@gmail.com>
2025-04-03 16:24:17 -0300

    Creating new Views

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,18 @@ def index(request):
 
 def test_view(request, number):
     return HttpResponse(f"Received number: {number}")
+
+def test_bash(request):
+    return HttpResponse("This is a Linux bash test.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
